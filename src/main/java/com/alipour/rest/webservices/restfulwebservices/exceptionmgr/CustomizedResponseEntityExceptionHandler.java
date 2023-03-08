6f2150352f635c84f393958e4a3b8f8516c966b1
eniac1989 <alipourpaniz@gmail.com>
2023-03-08 12:14:00 +0330

    1401-12-17

--- src/main/java/com/alipour/rest/webservices/restfulwebservices/exceptionmgr/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/alipour/rest/webservices/restfulwebservices/exceptionmgr/CustomizedResponseEntityExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.alipour.rest.webservices.restfulwebservices.exceptionmgr;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -25,5 +27,11 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
     public final ResponseEntity<ErrorDetails> handleUserNotFoundException(UserNotFoundException ex, WebRequest request) throws Exception {
         ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), "validation failed", ex.getBindingResult().toString());
+        return new ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/alipour/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/alipour/rest/webservices/restfulwebservices/user/User.java
@@ -1,5 +1,7 @@
 package com.alipour.rest.webservices.restfulwebservices.user;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 /**
@@ -9,8 +11,9 @@ public class User {
 
     private Integer id;
 
+    @Size(min = 2,message = "name should have at least 2 characters")
     private String name;
-
+    @Past
     private LocalDate birthDate;
 
     public User(Integer id, String name, LocalDate birthDate) {
