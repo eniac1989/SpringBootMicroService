fc27fa149121b360726dc0412b3ba6cfb4bec922
eniac1989 <alipourpaniz@gmail.com>
2023-03-11 10:00:56 +0330

    1401-12-20

--- src/main/java/com/alipour/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/alipour/rest/webservices/restfulwebservices/user/User.java
@@ -1,5 +1,7 @@
 package com.alipour.rest.webservices.restfulwebservices.user;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 /**
@@ -9,8 +11,10 @@ public class User {
 
     private Integer id;
 
+    @Size(min = 2,message = "Name should have at least 2 characters")
     private String name;
 
+    @Past(message = "Birth date should be in the past")
     private LocalDate birthDate;
 
     public User(Integer id, String name, LocalDate birthDate) {
