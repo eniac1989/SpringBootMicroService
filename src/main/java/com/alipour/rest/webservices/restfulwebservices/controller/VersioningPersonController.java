5ed7d2b83c377ee9c03f7b03326d78e110c0f033
eniac1989 <alipourpaniz@gmail.com>
2023-03-19 16:37:06 +0330

    1401-12-28

--- src/main/java/com/alipour/rest/webservices/restfulwebservices/controller/VersioningPersonController.java
+++ src/main/java/com/alipour/rest/webservices/restfulwebservices/controller/VersioningPersonController.java
@@ -7,6 +7,7 @@ import com.alipour.rest.webservices.restfulwebservices.user.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.file.Path;
 import java.util.List;
 
 @RestController
@@ -19,6 +20,36 @@ public class VersioningPersonController {
 
     @GetMapping(path = "v2/person")
     PersonV2 secondVersionOfGetPerson() {
+        return new PersonV2(new Name("Paniz","Alipour"));
+    }
+
+    @GetMapping(path = "person",params = "version=1")
+    PersonV1 firstVersionOfGetPersonWithParams() {
+        return new PersonV1("Paniz Alipour");
+    }
+
+    @GetMapping(path = "person",params = "version=2")
+    PersonV2 secondVersionOfGetPersonWithParams() {
+        return new PersonV2(new Name("Paniz","Alipour"));
+    }
+
+    @GetMapping(path = "person",headers = "X-API-VERSION=1")
+    PersonV1 firstVersionOfGetPersonWithHeaders() {
+        return new PersonV1("Paniz Alipour");
+    }
+
+    @GetMapping(path = "person",headers = "X-API-VERSION=2")
+    PersonV2 secondVersionOfGetPersonWithHeaders() {
+        return new PersonV2(new Name("Paniz","Alipour"));
+    }
+
+    @GetMapping(path = "person",produces = "application/vnd.company.app-v1+json")
+    PersonV1 firstVersionOfGetPersonAcceptHeaders() {
+        return new PersonV1("Paniz Alipour");
+    }
+
+    @GetMapping(path = "person",produces = "application/vnd.company.app-v2+json")
+    PersonV2 secondVersionOfGetPersonWithAcceptHeaders() {
         return new PersonV2(new Name("Paniz","Alipour"));
     }
 }
